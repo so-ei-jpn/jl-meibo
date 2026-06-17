dd2505fc8ac0902d595f3b4cb51e149066565192
so-ei-jpn <uniformprouni@gmail.com>
2026-06-17 17:03:21 +0900

    Update next.config.ts

--- jl-meibo/next.config.ts
+++ jl-meibo/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/jl-meibo",
-  output: "standalone",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
